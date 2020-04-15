82b2d3dbd9fb89f0e18c37acf9056a52b9f3a186
☯ IMPRESIVΞ <60080728+ImpresiveDEV@users.noreply.github.com>
2020-04-15 16:06:27 +0200

    Add files via upload

---
+++ Hanbot-SX V2 OpenSource/AlphaHanbot-SX/authentication/Globals.cs
@@ -0,0 +1,24 @@
+﻿namespace AlphaHanbot_SX.authentication
+{
+    internal class Globals
+    {
+        public static string LoginPHP = "<source>";
+        public static string RegisterPHP = "<source>";
+        public static string InfosPHP = "<source>";
+
+        public static int ReadLine = 0;
+
+        public static string Username = null;
+        public static string Password = null;
+        public static string Token = null;
+
+        public static string CheckStatus = null;
+        public static bool FinishedChecking = false;
+
+        public static string RegisterStatus = null;
+        public static bool FinishedRegister = false;
+
+        public static string SubscriptionLeft = null;
+        public static string SubscriptionType = null;
+    }
+}
